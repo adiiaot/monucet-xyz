3aebda03d813f678b04dc181968c8af7d6aadb83
berzan <berzan@monda.exchange>
2025-07-19 20:36:59 +0300

    Initialize API module.

---
+++ api/src/index.ts
@@ -0,0 +1,5 @@
+import { Elysia } from 'elysia'
+
+const app = new Elysia().get('/', () => 'Gmonad!').listen(3000)
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
