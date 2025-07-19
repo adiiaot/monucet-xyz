35d02c4eab5f489812b4369659f629cb7908eeaa
berzan <berzan@monda.exchange>
2025-07-20 02:32:46 +0300

    Create limiter test.

---
+++ api/test/limiter.test.ts
@@ -0,0 +1,14 @@
+import { test, expect } from 'bun:test'
+
+import { Limiter } from '../src/limiter'
+
+test('limiter', async () => {
+  const limiter = new Limiter(100)
+
+  expect(limiter.isSendable('hello')).toBe(0)
+  await new Promise((res) => setTimeout(res, 50))
+  expect(limiter.isSendable('hello')).toBe(1)
+
+  await new Promise((res) => setTimeout(res, 50))
+  expect(limiter.isSendable('hello')).toBe(0)
+})
